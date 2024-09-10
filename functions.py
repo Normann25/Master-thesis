db97273525548244e62b4ff0ee6f04c4b297afbf
LAU100100 <159791742+LAU100100@users.noreply.github.com>
2024-09-10 16:13:34 +0200

    edit

--- functions.py
+++ functions.py
@@ -37,3 +37,41 @@ def read_txt_acsm(path, parent_path, file_names, separation):
         new_dict[key] = df
     
     return new_dict
+
+def read_data(path, parent_path, time_label):
+    parentPath = os.path.abspath(parent_path)
+    if parentPath not in sys.path:
+        sys.path.insert(0, parentPath)
+
+    files = os.listdir(path)
+    data_dict = {}
+
+    for file in files:
+        name = file.split('.')[0]
+        with open(os.path.join(path, file)) as f:
+            df = pd.read_csv(f, sep = ';', decimal=',')
+            df = df.dropna()
+
+            df['PAH total'] = pd.to_numeric(df['PAH total'], errors = 'coerce')
+
+            df[time_label] = df[time_label].str.split().str[1] + pd.Timedelta('2 hours')
+            df['Time'] = pd.to_timedelta(df[time_label]).astype('timedelta64[s]')
+
+        data_dict[name] = df
+    
+    return data_dict
+
+def plot_PAH_ACSM(ax, data_dict, dict_keys):
+    for i, dict_key in enumerate(dict_keys):
+        df = data_dict[dict_key]
+        #for j, key in enumerate(df.keys()[1:]):
+        ax[i].plot(df['Time'], df['PAH total'], lw = 1)
+
+
+        formatter = FuncFormatter(lambda s, x: time.strftime('%H:%M', time.gmtime(s)))
+        ax[i].xaxis.set_major_formatter(formatter)
+        ax[i].tick_params(axis = 'both', which = 'major', direction = 'out', bottom = True, left = True, labelsize = 8)
+
+        ax[i].set_xlabel('Time', fontsize = 8)
+        ax[i].set_ylabel('PAH$_{est}$ / $\mu$g/m$^{3}$', fontsize = 8)
+        ax[i].set_title(dict_key, fontsize = 9)
