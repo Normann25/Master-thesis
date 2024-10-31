a34401097ed8d6415f45f6c1a9966bed5e853174
LAU100100 <159791742+LAU100100@users.noreply.github.com>
2024-10-31 13:19:20 +0100

    Update Laura.py
    New try

--- Laura.py
+++ Laura.py
@@ -1,6 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import sys
+from matplotlib.ticker import FuncFormatter
+import time
+from datetime import datetime
+import matplotlib.dates as mdates
+import linecache
+from iminuit import Minuit
+
+
 def read_CPC(path, parent_path, timelable):
-     parentPath = os.path.abspath(parent_path)
-     if parentPath not in sys.path:
-        sys.path.insert(0, parentPath)
+   parentPath = os.path.abspath(parent_path)
+   if parentPath not in sys.path:
+      sys.path.insert(0, parentPath)
+   
+   files = os.listdir(path)
+   new_dict = {}
+
+
+
+   #%%
+   parent = '../../../' 
+
+      
         
         
