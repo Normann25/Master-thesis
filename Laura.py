1398fb8a40747ebc28a7b79d7d8dccc9e54a4014
LAU100100 <159791742+LAU100100@users.noreply.github.com>
2024-10-31 14:36:17 +0100

    New

--- Laura.py
+++ Laura.py
@@ -11,19 +11,34 @@ import linecache
 from iminuit import Minuit
 
 
-def read_CPC(path, parent_path, timelable):
-   parentPath = os.path.abspath(parent_path)
-   if parentPath not in sys.path:
-      sys.path.insert(0, parentPath)
+def read_CPC(path):
    
    files = os.listdir(path)
    new_dict = {}
 
+   for file in files:
+      name = file.split('.')[0]
+      try:
+         with open(os.path.join(path, file)) as f:
+            df = pd.read_csv(f, sep = ',', skiprows=2)[:-2]
+            df['Time'] = pd.to_datetime(df['Time'])
+      
+      except KeyError:
+         with open(os.path.join(path, file)) as f:
+            df = pd.read_csv(f, sep = ',', skiprows=17)[:-2]
+            df['Time'] = pd.to_datetime(df['Time'])
+      new_dict[name] = df
+   return new_dict
+
+def format_timestamps(timestamps, old_format, new_format):
+    new_timestamps = []
+    for timestamp in timestamps:
+        old_datetime = datetime.strptime(timestamp, old_format)
+        new_datetime = old_datetime.strftime(new_format)
+        new_timestamps.append(new_datetime)
+    return pd.to_datetime(new_timestamps, format=new_format)
 
 
-   #%%
-   parent = '../../../' 
+
 
       
-        
-        
