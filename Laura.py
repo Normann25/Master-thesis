a22bcccc691b0a502353acfe8360f7efd07f3fc3
Normann25 <nan25-12@hotmail.dk>
2024-11-12 14:29:53 +0100

    update

--- Laura.py
+++ Laura.py
@@ -40,27 +40,35 @@ def format_timestamps(timestamps, old_format, new_format):
 
 # def plot_CPC_timeseries(ax, fig): 
 
-
+def format_single_timestamp(column, from_format, to_format):
+    timestamp = pd.to_datetime(column, format=from_format)
+    return timestamp.strftime(to_format) if isinstance(timestamp, pd.Timestamp) else timestamp.dt.strftime(to_format)
 
 def read_LCS_KU_data(path, time_label, names):
+    
+    files = os.listdir(path)
+    data_dict = {}
 
-   files = os.listdir(path)
-   data_dict = {}
+    for name in names:
+        for file in files:
+            if name in file:
+                with open(os.path.join(path, file)) as f:
+                    df = pd.read_table(f, sep = ',')
 
-   for name in names:
-      for file in files:
-         if name in file:
-            with open(os.path.join(path, file)) as f:
-               df = pd.read_table(f, sep = ',')
+                Timestamps = []
+                for time in df[time_label]:
+                    try:
+                        timestamp = pd.to_datetime(time, format = '%Y-%m-%d %H:%M:%S')
+                        Timestamps.append(timestamp)
+                    except ValueError:
+                        timestamp = format_single_timestamp(time, '%Y-%m-%d %H:%M:%S.%f', '%d/%m/%Y %H:%M:%S')
+                        Timestamps.append(timestamp)
 
-               display(df)
+                df[time_label] = Timestamps
 
-               # df[time_label] = pd.to_datetime(df[time_label])
-               df[time_label] = format_timestamps(df[time_label], '%Y-%m-%d %H:%M:%S.%f', '%d/%m/%Y %H:%M')
+                data_dict[name] =df 
 
-            data_dict[name] =df 
-   
-   return data_dict
+    return data_dict
 
 
 def read_LCS_data_OPC5000(path, time_label, names, sep):
@@ -92,19 +100,26 @@ def read_LCS_data_LUND(path, time_label, names):
     for name in names:
         for file in files:
             if name in file and file.endswith('.xlsx'): 
-                try:  
-                    df = pd.read_excel(os.path.join(path, file))
-                    df[df.keys()[0]] = format_timestamps(df[df.keys()[0]], '%Y-%m-%d %H:%M:%S', '%d/%m/%Y %H:%M')
-                    dates = []
-                    for date in str(df[df.keys()[0]]).split(' ')[0].unique():
-                        dates.append(date)
-                    for date, sensor in zip(dates, df['Entity Name'].unique()):
+
+                # try:  
+                df = pd.read_excel(os.path.join(path, file), decimal=',')
+
+                dates = []
+                for time in df[time_label]:
+                    date = str(time).split(' ')[0]
+                    dates.append(date)
+                df['Date'] = dates
+
+                df[time_label] = format_timestamps(df[time_label], '%Y-%m-%d %H:%M:%S', '%d/%m/%Y %H:%M')
+
+                for date in df['Date'].unique():
+                    for sensor in df['Entity Name'].unique():
                         mask = df['Entity Name'] == sensor
                         new_df = df[mask].reset_index()
                         df_name = str(date) + ' ' + str(sensor)
                         data_dict[df_name] = new_df.drop('index', axis = 1)
-                except Exception as e:
-                    print(f"An error occurred while processing {file}: {e}")
+                # except Exception as e:
+                #     print(f"An error occurred while processing {file}: {e}")
 
     return data_dict
 
