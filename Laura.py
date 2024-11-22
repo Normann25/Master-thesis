2250c873da2817e841bc19610df1317fe06e93a1
LAU100100 <159791742+LAU100100@users.noreply.github.com>
2024-11-22 13:29:23 +0100

    New

--- Laura.py
+++ Laura.py
@@ -54,7 +54,9 @@ def read_LCS_KU_data(path, time_label, names):
             if name in file:
                 with open(os.path.join(path, file)) as f:
                     df = pd.read_table(f, sep = ',')
-
+                    keys = ['PM5000S_2_PN0.3', 'PM5000S_2_PN0.5','PM5000S_2_PN1','PM5000S_2_PN2.5','PM5000S_2_PN5','PM5000S_2_PN10']
+                    for key in keys: 
+                        df[key] = pd.to_numeric(df[key]) / 1000
                 Timestamps = []
                 for time in df[time_label]:
                     try:
@@ -83,9 +85,12 @@ def read_LCS_data_OPC5000(path, time_label, names, sep):
                df = pd.read_table(f, sep = sep)
 
                df[time_label] = format_timestamps(df[time_label], '%Y-%m-%d %H:%M:%S', '%d/%m/%Y %H:%M')
+               keys = ['PM5000S_2_PN0.3', 'PM5000S_2_PN0.5','PM5000S_2_PN1','PM5000S_2_PN2.5','PM5000S_2_PN5','PM5000S_2_PN10']
+               for key in keys: 
+                   df[key] = pd.to_numeric(df[key]) / 1000
 
             data_dict[name] =df 
-   
+
    return data_dict
 
 
@@ -122,4 +127,78 @@ def read_LCS_data_LUND(path, time_label, names):
                 #     print(f"An error occurred while processing {file}: {e}")
 
     return data_dict
+
+
+def plot_timeseries(fig, ax, df, df_keys, bin_edges, datatype, timestamps):
+
+    start_time = pd.to_datetime(timestamps[0])
+    end_time = pd.to_datetime(timestamps[1])
+
+    time = pd.to_datetime(df['Time'])
+
+    time_filter = (time >= start_time) & (time <= end_time)
+
+    filtered_time = np.array(time[time_filter])
+
+    new_df = pd.DataFrame({'Time': filtered_time})
+
+    for key in df_keys:
+        conc = np.array(df[key])
+        conc = pd.to_numeric(conc, errors='coerce')
+        filtered_conc = conc[time_filter]
+
+        new_df[key] = filtered_conc
+
+    data = np.array(new_df[new_df.keys()[1:]])
+
+    # Set the upper and/or lower limit of the color scale based on input
+    y_min = np.nanmin(data)
+    y_max = np.nanmax(data)
+
+    # Generate an extra time bin, which is needed for the meshgrid
+    dt = filtered_time[1]-filtered_time[0]
+    new_time = filtered_time - dt
+    new_time = np.append(new_time, new_time[-1]+dt)
+
+    # generate 2d meshgrid for the x, y, and z data of the 3D color plot
+    y, x = np.meshgrid(bin_edges, new_time)
+
+    # Fill the generated mesh with particle concentration data
+    p1 = ax[0].pcolormesh(x, y, data, cmap='jet',vmin=y_min, vmax=y_max,shading='flat')
+
+    ax[0].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=-45, ha="left")
+    ax[0].set_xlabel("Time, HH:MM")
+    plt.subplots_adjust(hspace=0.05)
+        
+    # Make the y-scal logarithmic and set a label
+    ax[0].set_yscale("log")
+    ax[0].set_ylabel("Dp, nm")
+
+    total_conc = new_df.iloc[:,1:].sum(axis=1)
+    ax[1].plot(new_df['Time'], total_conc, lw = 1, color = 'r')
+
+    ax[1].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+    ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=-45, ha="left")
+    ax[1].set_xlabel("Time, HH:MM")
+    plt.subplots_adjust(hspace=0.05)
+
+    # Insert coloarbar and label it
+    col = fig.colorbar(p1, ax=ax[0])
+    if datatype == "number":
+        col.set_label('dN, cm$^{-3}$')
+        ax[1].set_ylabel('dN, cm$^{-3}$')
+    elif datatype == "mass":
+        col.set_label('dm, $\mu$g/m$^{3}$')
+        ax[1].set_ylabel('dm, $\mu$g/m$^{3}$')
+    elif datatype == "normed":
+        col.set_label('dN/dlogDp, cm$^{-3}$')
+        ax[1].set_ylabel('dN/dlogDp, cm$^{-3}$')
+
+    # Set ticks on the plot to be longer
+    ax[0].tick_params(axis="y",which="both",direction='out')
+    ax[1].tick_params(axis="y",which="both",direction='out')
+
+
+
 
